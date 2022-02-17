16fc3d0b330a839ef6b80b3c020262beda0c03dd
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-02-17 23:31:54 +0530

    Update lichessbot.py

--- lichessbot.py
+++ lichessbot.py
@@ -121,15 +121,15 @@ def play_game(li, game_id, engine_factory, user_profile, config):
     #Initial response of stream will be the full game info. Store it
     initial_state = json.loads(next(lines).decode('utf-8'))
     game = model.Game(initial_state, user_profile["username"], li.baseUrl, config.get("abort_time", 20))
-    timelim=game.state["btime"]/1000
+    timelim=game.state["btime"]/100
     timelim=timelim/15
     if timelim>=0.3 and timelim<=2:
         bullet=True
-    time=round(timelim/150*60,1)
-    if time>6:
-        time=6
-    elif time<0.3:
-        time=0.2
+    time=round(timelim/50*20,1)
+    if time>9:
+        time=2
+    elif time<0.1:
+        time=0.1
     if bullet:
         time=0.1
     board = setup_board(game)
